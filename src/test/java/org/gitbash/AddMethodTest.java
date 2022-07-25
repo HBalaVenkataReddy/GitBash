85107d216cc7cf1160da5d6d6b9accbca6e28b47
HBalaVenkataReddy <balahanumanthuselenium@gmail.com>
2022-07-25 11:52:02 +0530

    my first commit

---
+++ src/test/java/org/gitbash/AddMethodTest.java
@@ -0,0 +1,12 @@
+package org.gitbash;
+
+public class AddMethodTest {
+
+	public static void main(String[] args) 
+	{
+		int a=10;
+		int b=20;
+		int c=a+b;
+		System.out.println("Add Sum"+c);
+	}
+}
